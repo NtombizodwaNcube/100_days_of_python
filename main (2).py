9cb897d008ef16f79b91133213b34c1b1fd53f85
ZoeN <81691687+NtombizodwaNcube@users.noreply.github.com>
2023-01-01 15:09:28 +0200

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input('You\'re at a crossroad, where do you want to go? Type "left" or "right". ').lower()
+if choice1 == "right":
+  print("You walked into quicksand.Game Over!.")
+else:
+  print("Congratulations, you are one step closer to finding your treasure")
+
+choice2 = input('Would you like to "swim" or "wait"?').lower()
+if choice2 == "swim": 
+  print("Oops, you've been eaten by a shark. Game Over!")
+else:
+  print("Congratulations, you are still in the game ")
+
+choice3 = input('Which door would do you want to open? "red door" or "blue door" or "yellow door"  ').lower()
+if choice3 == "red door": 
+  print('You have been burned by fire, Game over!')
+elif choice3 == "blue door":
+  print("You have been eaten by beasts. Game over!")
+else:
+  print("Congratulations, You Win!")
